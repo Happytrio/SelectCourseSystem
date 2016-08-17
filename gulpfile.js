748c21646cb3e989bc15a6201173758592853e11
tianmao <971070489@qq.com>
2016-08-17 18:09:27 +0800

    gulp set

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,11 @@
 //导入工具包 require('node_modules里对应模块')
 var gulp = require('gulp'), //本地安装gulp所用到的地方
-    uglify = require('gulp-uglify');
+	less = require('gulp-less');
+    
+
+gulp.task('lesson',function(){
+	gulp.src('framework/less/lessonList.less')
+	.pipe(less())
+	.pipe(gulp.dest('style/lessonCss'));
+});
+
